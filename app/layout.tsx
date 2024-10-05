e2908f8b6c26b97293a56528fc5cfff3788b7e5d
juju-anselum <anselumjuju@gmail.com>
2024-10-06 00:35:32 +0530

    Updating meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,33 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Delicio',
-  description: 'A delicious recipe app.',
+  description: 'Delicio is a delicious recipe app, that recommends recipes based on preferences.',
+  openGraph: {
+    url: 'https://delicioo.vercel.app/',
+    type: 'website',
+    title: 'Delicio',
+    description: 'Delicio is a delicious recipe app, that recommends recipes based on preferences.',
+    images: [
+      {
+        url: 'https://delicioo.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://delicioo.vercel.app/',
+    title: 'Delicio',
+    description: 'Delicio is a delicious recipe app, that recommends recipes based on preferences.',
+    images: [
+      {
+        url: 'https://delicioo.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
