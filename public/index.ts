4ec12f9d94cec741205b6a93134750c47081f74e
juju-anselum <anselumjuju@gmail.com>
2024-10-18 22:48:07 +0530

    Image path fixed

--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,3 @@
-export { default as aiBurgerImage } from "./assets/aiBurgerImage.png";
+export { default as aiBurgerImage } from "./assets/aiBurgerImage.jpg";
 export { default as Logo } from "./assets/Logo.svg";
 export { default as CurvedLogo } from "./assets/CurvedLogo.svg";
