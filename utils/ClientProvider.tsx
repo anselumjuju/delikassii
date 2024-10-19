b49808e6a56c83128089fe8fe06e58b203d7ec60
juju-anselum <anselumjuju@gmail.com>
2024-10-19 22:50:40 +0530

    Stale time increased

--- utils/ClientProvider.tsx
+++ utils/ClientProvider.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000,
+      staleTime: 5 * 60 * 1000 * 12 * 24 * 10,
       refetchInterval: false,
       refetchOnWindowFocus: false,
     },
