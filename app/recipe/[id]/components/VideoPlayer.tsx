2ba511ca5d090b7ffa76d423d9548f35762d12d1
juju-anselum <anselumjuju@gmail.com>
2024-12-31 18:19:00 +0530

    Add unoptimized prop to Image component in VideoPlayer

--- app/recipe/[id]/components/VideoPlayer.tsx
+++ app/recipe/[id]/components/VideoPlayer.tsx
@@ -10,7 +10,7 @@ interface VideoPlayerProps {
 const VideoPlayer = ({ thumbnail_url, thumbnail_alt_text, video_url }: VideoPlayerProps) => {
   return (
     <div className='w-full aspect-video h-full max-h-[75vh]'>
-      <Image src={thumbnail_url} alt={thumbnail_alt_text} width={300} height={300} className='w-full h-full object-cover rounded-lg' />
+      <Image src={thumbnail_url} alt={thumbnail_alt_text} width={300} height={300} className='w-full h-full object-cover rounded-lg' unoptimized />
     </div>
   );
 };
