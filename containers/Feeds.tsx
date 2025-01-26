8804fae34cbc24c60eead451d6d8b6ce713cc296
anselumjuju <anselumjuju@gmail.com>
2025-01-26 17:08:17 +0530

    fix: image not showing in recipe cards

--- containers/Feeds.tsx
+++ containers/Feeds.tsx
@@ -12,17 +12,17 @@ const Feeds = ({ feeds }: feedDataProps) => {
     .filter((data: FeedDataInterface) => data.name && Array.isArray(data.items) && data.items.length > 1)
     .sort((a: FeedDataInterface, b: FeedDataInterface) => (b.items?.length || 0) - (a.items?.length || 0))
     .slice(0, -1);
-
   return (
     <div className='w-full space-y-10'>
       {data &&
         data.map((recipe: FeedDataInterface, idx) => {
           const CardComponent = idx % 2 == 0 ? RecipeCard2 : RecipeCard3;
+          if (recipe.items.some((item) => item.thumbnail_url === '')) return null;
           return (
             <div key={recipe.name} className='space-y-6'>
               <h1 className='text-2xl md:text-3xl font-bold'>{recipe.name}</h1>
               <div className='w-full flex items-stretch justify-normal gap-4 lg:gap-6 overflow-auto no-scroll-bar'>
-                {Array.isArray(recipe.items) && recipe.items?.map((item) => <CardComponent recipe={item} key={item.id} />)}
+                {Array.isArray(recipe.items) && recipe.items.map((item) => <CardComponent recipe={item} key={item.id} />)}
               </div>
             </div>
           );
